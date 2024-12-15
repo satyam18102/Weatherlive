84293485c76ec67dfca0414851078352c2fed1ff
Satyam Sinha <satyamsinha340@gmail.com>
2024-12-15 12:57:41 +0530

    First Commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { Provider } from './components/ui/provider.jsx'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import './index.css'
+import './index.css'
 import Demo from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
